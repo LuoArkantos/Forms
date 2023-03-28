e7da42c894b62740609a4399a64784a1d78f5f19
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-03-28 19:45:53 -0300

    Validacao MessageBox se vazio, incompleto, confirmacao de validacao

--- CursoWindowsForms01/Frm_ValidaCPF2.cs
+++ CursoWindowsForms01/Frm_ValidaCPF2.cs
@@ -16,5 +16,50 @@ namespace CursoWindowsForms01
         {
             InitializeComponent();
         }
+
+        private void Frm_ValidaCPF2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            string vConteudo = Msk_CPF.Text;
+            vConteudo = vConteudo.Replace(".", "").Replace("-", "");
+            vConteudo = vConteudo.Trim();
+            
+            if (vConteudo == "")
+            {
+                MessageBox.Show("CPF VAZIO", "Validação de CPF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (vConteudo.Length != 11)
+                {
+                    MessageBox.Show("CPF INCOMPLETO", "Validação de CPF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (MessageBox.Show("Deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) //Abre uma janela com opções SIM e NAO, quando clicado em SIM, executa o corpo da condicional
+                    {
+                        bool validaCpf = false;
+                        validaCpf = Cls_Uteis.Valida(Msk_CPF.Text);
+                        if (validaCpf == true)
+                        {
+                            MessageBox.Show("CPF VÁLIDO!", "Validação de CPF", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("CPF INVÁLIDO!", "Validação de CPF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }                
+            }            
+        }
     }
 }
