6805c750986e41238c1be73085c8ac09500a6859
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-03-29 21:23:53 -0300

    janela MDI - Deixando de usar DialogBox para exibir forms na janela home

---
+++ CursoWindowsForms01/Frm_PrincipalMenu_MDI.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace CursoWindowsForms01
+{
+    partial class Frm_PrincipalMenu_MDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_PrincipalMenu_MDI));
+            this.Mnu_Principal = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.demonstraçãoKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helloWorldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.máscaraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valídaCPFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valídaCPF2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valídaSenhaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Mnu_Principal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Mnu_Principal
+            // 
+            this.Mnu_Principal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.windowsToolStripMenuItem});
+            this.Mnu_Principal.Location = new System.Drawing.Point(0, 0);
+            this.Mnu_Principal.Name = "Mnu_Principal";
+            this.Mnu_Principal.Size = new System.Drawing.Size(800, 24);
+            this.Mnu_Principal.TabIndex = 0;
+            this.Mnu_Principal.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.demonstraçãoKeyToolStripMenuItem,
+            this.helloWorldToolStripMenuItem,
+            this.máscaraToolStripMenuItem,
+            this.valídaCPFToolStripMenuItem,
+            this.valídaCPF2ToolStripMenuItem,
+            this.valídaSenhaToolStripMenuItem});
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.novoToolStripMenuItem.Text = "Novo";
+            // 
+            // demonstraçãoKeyToolStripMenuItem
+            // 
+            this.demonstraçãoKeyToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("demonstraçãoKeyToolStripMenuItem.Image")));
+            this.demonstraçãoKeyToolStripMenuItem.Name = "demonstraçãoKeyToolStripMenuItem";
+            this.demonstraçãoKeyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.K)));
+            this.demonstraçãoKeyToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.demonstraçãoKeyToolStripMenuItem.Text = "Demonstração &Key";
+            this.demonstraçãoKeyToolStripMenuItem.Click += new System.EventHandler(this.demonstraçãoKeyToolStripMenuItem_Click);
+            // 
+            // helloWorldToolStripMenuItem
+            // 
+            this.helloWorldToolStripMenuItem.Image = global::CursoWindowsForms01.Properties.Resources.Frm_HelloWorld2;
+            this.helloWorldToolStripMenuItem.Name = "helloWorldToolStripMenuItem";
+            this.helloWorldToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.helloWorldToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.helloWorldToolStripMenuItem.Text = "Hello &World";
+            this.helloWorldToolStripMenuItem.Click += new System.EventHandler(this.helloWorldToolStripMenuItem_Click);
+            // 
+            // máscaraToolStripMenuItem
+            // 
+            this.máscaraToolStripMenuItem.Image = global::CursoWindowsForms01.Properties.Resources.Frm_Mascara2;
+            this.máscaraToolStripMenuItem.Name = "máscaraToolStripMenuItem";
+            this.máscaraToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.M)));
+            this.máscaraToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.máscaraToolStripMenuItem.Text = "&Máscara";
+            this.máscaraToolStripMenuItem.Click += new System.EventHandler(this.máscaraToolStripMenuItem_Click);
+            // 
+            // valídaCPFToolStripMenuItem
+            // 
+            this.valídaCPFToolStripMenuItem.Image = global::CursoWindowsForms01.Properties.Resources.Frm_ValidaCPF3;
+            this.valídaCPFToolStripMenuItem.Name = "valídaCPFToolStripMenuItem";
+            this.valídaCPFToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.valídaCPFToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.valídaCPFToolStripMenuItem.Text = "Valída &CPF";
+            this.valídaCPFToolStripMenuItem.Click += new System.EventHandler(this.valídaCPFToolStripMenuItem_Click);
+            // 
+            // valídaCPF2ToolStripMenuItem
+            // 
+            this.valídaCPF2ToolStripMenuItem.Image = global::CursoWindowsForms01.Properties.Resources.Frm_ValidaCPF22;
+            this.valídaCPF2ToolStripMenuItem.Name = "valídaCPF2ToolStripMenuItem";
+            this.valídaCPF2ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.valídaCPF2ToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.valídaCPF2ToolStripMenuItem.Text = "Valída C&PF 2";
+            this.valídaCPF2ToolStripMenuItem.Click += new System.EventHandler(this.valídaCPF2ToolStripMenuItem_Click);
+            // 
+            // valídaSenhaToolStripMenuItem
+            // 
+            this.valídaSenhaToolStripMenuItem.Image = global::CursoWindowsForms01.Properties.Resources.Frm_ValidaSenha2;
+            this.valídaSenhaToolStripMenuItem.Name = "valídaSenhaToolStripMenuItem";
+            this.valídaSenhaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.valídaSenhaToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.valídaSenhaToolStripMenuItem.Text = "Valída &Senha";
+            this.valídaSenhaToolStripMenuItem.Click += new System.EventHandler(this.valídaSenhaToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // windowsToolStripMenuItem
+            // 
+            this.windowsToolStripMenuItem.Name = "windowsToolStripMenuItem";
+            this.windowsToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.windowsToolStripMenuItem.Text = "Windows";
+            // 
+            // Frm_PrincipalMenu_MDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Mnu_Principal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.Mnu_Principal;
+            this.Name = "Frm_PrincipalMenu_MDI";
+            this.Text = "Principal";
+            this.Mnu_Principal.ResumeLayout(false);
+            this.Mnu_Principal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Mnu_Principal;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem demonstraçãoKeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helloWorldToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem máscaraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem valídaCPFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem valídaCPF2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem valídaSenhaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowsToolStripMenuItem;
+    }
+}
---
+++ CursoWindowsForms01/Frm_PrincipalMenu_MDI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms01
+{
+    public partial class Frm_PrincipalMenu_MDI : Form
+    {
+        public Frm_PrincipalMenu_MDI()
+        {
+            InitializeComponent();
+        }
+
+        private void demonstraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey f = new Frm_DemonstracaoKey();
+            f.MdiParent = this; //Diz que ESTE (Frm_PrincipalMenu_MDI) é a janela PAI do formulario q vai ser aberto, assim abrindo o form dentro da janela principal
+            f.Show(); 
+        }
+
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_HelloWorld f = new Frm_HelloWorld();
+            f.MdiParent = this; //Diz que ESTE (Frm_PrincipalMenu_MDI) é a janela PAI do formulario q vai ser aberto, assim abrindo o form dentro da janela principal
+            f.Show();
+        }
+
+        private void máscaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara f = new Frm_Mascara();
+            f.MdiParent = this; //Diz que ESTE (Frm_PrincipalMenu_MDI) é a janela PAI do formulario q vai ser aberto, assim abrindo o form dentro da janela principal
+            f.Show();
+        }
+
+        private void valídaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF f = new Frm_ValidaCPF();
+            f.MdiParent = this; //Diz que ESTE (Frm_PrincipalMenu_MDI) é a janela PAI do formulario q vai ser aberto, assim abrindo o form dentro da janela principal
+            f.Show();
+        }
+
+        private void valídaCPF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF2 f = new Frm_ValidaCPF2();
+            f.MdiParent = this; //Diz que ESTE (Frm_PrincipalMenu_MDI) é a janela PAI do formulario q vai ser aberto, assim abrindo o form dentro da janela principal
+            f.Show();
+        }
+
+        private void valídaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha f = new Frm_ValidaSenha();
+            f.MdiParent = this; //Diz que ESTE (Frm_PrincipalMenu_MDI) é a janela PAI do formulario q vai ser aberto, assim abrindo o form dentro da janela principal
+            f.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
